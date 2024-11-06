1183f80e314d4477435a8946a9f09b3bf1cb3ad3
Igboama Winifred <winiigboama@gmail.com>
2024-11-06 13:03:51 +0100

    explain custom validators

--- music_store/music/models.py
+++ music_store/music/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+def validate_debut_year(year):
+    current_year = timezone.now().year
+    print(current_year)
+    if year < 1700 or year > current_year:
+        raise ValidationError("Invalid debut year")
 
 # Create your models here.
 class Artist(models.Model):
     name = models.CharField(max_length=255)
-    debut_year = models.IntegerField()
+    debut_year = models.IntegerField(validators=[validate_debut_year])
+
 
     def __str__(self):
         return self.name
