28b21426ea06821cbed1f89ce94a99e08edc2841
Igboama Winifred <winiigboama@gmail.com>
2024-11-06 12:30:13 +0100

    explain detail view

--- lms/authors/urls.py
+++ lms/authors/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "authors"
+
 urlpatterns = [
     path("all-authors/", views.all_authors, name="all_authors"),
     path('authors-books/', views.authors_books_view, name="authors_books"),
+    path('author-detail/<int:author_pk>', views.author_detail, name="author_detail"),
 ]
--- lms/authors/views.py
+++ lms/authors/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Author
 from library.models import Book
 
 # Create your views here.
 def all_authors(request):
-    return render(request, "authors/all-authors.html")
+    authors = Author.objects.all()
+    context = {"all_authors": authors}
+    return render(request, "authors/all-authors.html", context)
 
 def authors_books_view(request):
     authors = Author.objects.order_by("-date_of_birth")
@@ -15,3 +17,9 @@ def authors_books_view(request):
         "all_books": books,
     }
     return render(request, "authors/authors_books.html", context)
+
+
+def author_detail(request, author_pk):
+    author = get_object_or_404(Author, pk=author_pk)
+    context = {"author": author}
+    return render(request, "authors/author-detail.html", context)
