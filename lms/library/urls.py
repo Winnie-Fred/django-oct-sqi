273454dbe13c8056f9a4f0c65872d8896dc9b154
Igboama Winifred <winiigboama@gmail.com>
2024-11-12 12:18:59 +0100

    introduce django forms

--- lms/library/urls.py
+++ lms/library/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "library"
+
 urlpatterns = [
     path("home/", views.home, name="home"),
+    path("create-book/", views.create_book, name="create_book"),
 ]
--- lms/library/views.py
+++ lms/library/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import BookForm
 
 # Create your views here.
 def home(request):
     return render(request, "library/home.html")
 
-# Create a view called all_authors in the authors app's views.py.
-# It should return a rendered html page called 'all-authors.html'.
-# Put that html file in a folder called authors
-
-
-# 1. Create a folder called music_store
-# 2. Create a django project config inside - django-admin startproject config .
-# 3. Create an app called music
-# 4. Create a view called favorites
-# 5. Create a urls.py file in the music app
-# 6. Add the url to link to the favorites view 
-# 7. Include the url in the general project's config.urls
-# 8. Start the development server and go to the view in the browser.
+def create_book(request):
+    form = BookForm()
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("library:home")
+        
+    context = {"book_create_form": form}
+    
+    return render(request, "library/create-book.html", context)
