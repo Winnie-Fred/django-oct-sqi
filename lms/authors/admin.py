b5fdc6c0470f5e69d90ffaa096bb6230b197c47e
Igboama Winifred <winiigboama@gmail.com>
2024-11-13 10:55:56 +0100

    add Book inline form to Author in django admin

--- lms/authors/admin.py
+++ lms/authors/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Author
+from library.models import Book
 
 # Register your models here.
-admin.site.register(Author)
+
+class BookInline(admin.TabularInline):  # or admin.StackedInline
+    model = Book
+    extra = 1  # Number of empty forms to display in the inline (1 by default)
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
+
+admin.site.register(Author, AuthorAdmin)
