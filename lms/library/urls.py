3c1573dad264ae0b4c3af851095a87312e23ed93
Igboama Winifred <winiigboama@gmail.com>
2024-11-19 12:39:34 +0100

    teach update and delete with lms project

--- lms/library/urls.py
+++ lms/library/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("create-book/", views.create_book, name="create_book"),
     path('create-book-manually/', views.create_book_manual, name="create_book_manual"),
+    path("list-of-books/", views.book_list, name="all_books"),
+    path("book/<int:book_pk>", views.book_detail, name="book_detail"),
+    path("update-book/<int:id>", views.update_book, name="update_book"),
+    path("confirm-delete/<int:book_id>", views.confirm_delete, name="confirm_delete"),
+    path("delete-book/<int:book_id>", views.delete_book, name="delete_book"),
 ]
--- lms/library/views.py
+++ lms/library/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from authors.models import Author
+from .models import Book
 from .forms import BookForm
 
 # Create your views here.
@@ -30,3 +31,36 @@ def create_book_manual(request):
         
     context = {"form": form, "authors": authors}
     return render(request, "library/create-book-manual.html", context)
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "library/book-list.html", {"all_the_books": books})
+
+def book_detail(request, book_pk):
+    book = get_object_or_404(Book, pk=book_pk)
+    return render(request, "library/book-detail.html", {"book": book})
+
+def update_book(request, id):
+    book = get_object_or_404(Book, pk=id)
+    form = BookForm(instance=book)
+    if request.method == "POST":
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("library:all_books")
+    context = {
+        "book": book,
+        "form": form,
+    }
+    return render(request, "library/book-update.html", context)
+
+def confirm_delete(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    context = {"book": book}
+    return render(request, "library/confirm-delete.html", context)
+
+def delete_book(request, book_id):
+    if request.method == "POST":
+        book = get_object_or_404(Book, pk=book_id)
+        book.delete()
+    return redirect("library:all_books")
